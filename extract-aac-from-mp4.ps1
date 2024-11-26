6c412b26ff02c8d1abca5ffa16476fef74dbf9bc
Thomas Harold <tgh@tgharold.com>
2024-11-26 05:43:21 -0500

    semi-working concept

--- extract-aac-from-mp4.ps1
+++ extract-aac-from-mp4.ps1
@@ -39,8 +39,20 @@ C:\Reports\2009\January.csv
 #>
 
 param (
-    [Parameter(Position = 0, Mandatory = $True)][ValidateNotNullorEmpty()][string]$InputPath, 
-    [Parameter(Position = 1, Mandatory = $True)][ValidateNotNullorEmpty()][string]$OutPutPath
+    [Parameter(Position = 0, Mandatory = $True)]
+    [ValidateNotNullorEmpty()]
+    [string]
+    $InputPath, 
+
+    [Parameter(Position = 1, Mandatory = $True)]
+    [ValidateNotNullorEmpty()]
+    [string]
+    $OutPutPath,
+
+    [parameter(Mandatory=$false)]
+    [ValidateRange(1, [int]::MaxValue)]
+    [int]
+    $Sample
 )
 
 Function Test-CommandExistsOrStop {
@@ -79,14 +91,19 @@ function Test-PathOrStop {
 function Get-MP4Metadata {
     param (
         [Parameter(Mandatory = $true)]
-        [string]$FilePath
+        [string]$filePath
     )
 
     Test-CommandExistsOrStop $FFProbeCommand
-    $json = & $FFProbeCommand -v quiet -print_format json -show_format -show_streams $FilePath 
+    
+    Write-Verbose "Get-MP4Metadata: $($filePath)"
+    if (!(Test-Path -Path $filePath -PathType Leaf)) {
+        Write-Error "File '$($filePath)' does not exist!"
+        exit 1
+    }
 
-    $metadata = ConvertFrom-Json $json
-
+    $json = & $FFProbeCommand -pretty -v quiet -print_format json -show_error -show_chapters -show_format -show_streams "$filePath"
+    $metadata = $json | ConvertFrom-Json -Depth 10
     return $metadata
 }
 
@@ -105,4 +122,74 @@ Test-CommandExistsOrStop $FFMPEGCommand
 $MP4FileList = Get-ChildItem $InputPath -Filter *.mp4
 Write-Host "Found $($MP4FileList.Count) *.mp4 files."
 
+if ($Sample) {
+    Write-Host "Sampling only $($Sample) files."
+    $MP4FileList = $MP4FileList | Get-Random -Count $Sample
+}
 
+foreach ($mp4File in $MP4FileList) {
+    Write-Verbose "-----"
+
+    Write-Verbose "File: $($mp4File.Name)"
+    $mp4Metadata = Get-MP4Metadata $mp4File.FullName
+    #Write-Host $mp4Metadata
+    $jsonFile = [System.IO.Path]::ChangeExtension($mp4File, "json")
+    Write-Verbose "Create JSON file."
+    $mp4Metadata | Out-File -Encoding utf8NoBOM -FilePath $jsonFile
+
+    #Write-Host ($mp4Metadata | Format-Table | Out-String)
+    #$mp4Metadata | Get-Member -MemberType NoteProperty
+
+    $streams = $mp4Metadata.streams
+    Write-Verbose "Found $($streams.Count) streams."
+    $audioStreams = $streams | Where-Object -Property codec_type -EQ 'audio'
+    Write-Verbose "Found $($audioStreams.Count) audio streams."
+    foreach ($stream in $audioStreams) {
+        Write-Debug "  audio stream($($stream.index)): $($stream.codec_name) codec_type=$($stream.codec_type) bit_rate=$($stream.bit_rate) sample_rate=$($stream.sample_rate)"
+    }
+
+    $aacStreams = $audioStreams | Where-Object -Property codec_name -EQ 'aac'
+    if ($aacStreams.Count -EQ 1) {
+        $aacStream = $aacStreams[0]
+        Write-Host $aacStream
+        $aacFilePath = [System.IO.Path]::ChangeExtension($mp4File, ".m4a")
+        Write-Host $aacFilePath
+
+        if (!(Test-Path -PathType Leaf -Path $aacFilePath)) {
+
+            $streamIndex = $aacStream.index
+            if (![int]::TryParse($streamIndex, [ref]$null)) {
+                Write-Error "streamIndex ($(streamIndex)) is not a number!"
+                exit 1
+            }
+            $streamIndex = [int]$streamIndex
+            if ($streamIndex -lt 0 -or $streamIndex -gt 255) {
+                Write-Error "streamIndex ($(streamIndex)) is out of range!"
+                exit 1
+            }
+    
+            Write-Verbose "Extract stream ($($streamIndex)) to $($aacFilePath)"
+            & $FFMPEGCommand -i "$($mp4File.FullName)" -map "0:$($streamIndex)" -c copy "$($aacFilePath)"
+    
+            # ffmpeg -i input-video.avi -vn -acodec copy output-audio.aac
+            # ffmpeg -i input.mkv -map 0:a:3 -c copy output.m4a
+        }
+        else {
+            Write-Verbose "Skipping, file exists."
+        }
+    }
+    elseif ($aacStreams -GT 1) {
+        Write-Host "Multiple streams found."
+        foreach($aacStream in $aacStreams) {
+            Write-Host $aacStream
+            $aacFilePath = [System.IO.Path]::ChangeExtension($mp4File, ".$($aacStream.index).m4a")
+            Write-Host $aacFilePath
+        }
+    }
+    else {
+        Write-Verbose "No AAC streams found."
+    }
+}
+
+Write-Verbose "-----"
+Write-Host "Finished."
